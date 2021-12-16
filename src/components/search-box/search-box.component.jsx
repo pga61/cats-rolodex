ead94eb24b4fccc3a0cbfe8ff767376b709e4883
pga61 <p.gaspel@orange.fr>
2021-12-16 17:44:12 +0100

    change search placeholder

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import './search-box.styles.css';
+import "./search-box.styles.css";
 
-export const SearchBox = props => (
+export const SearchBox = (props) => (
   <input
-    className='search-box'
-    type='search'
-    placeholder='search monsters'
+    className="search-box"
+    type="search"
+    placeholder="search cats..."
     onChange={props.onSearchChange}
   />
 );
